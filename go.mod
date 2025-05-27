ead9661a5323fa5b23c98ca18c9aa0f279febd88
Viacheslav Poturaev <vearutop@gmail.com>
2025-05-27 12:35:52 +0200

    Serve version info with HTTP

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/xiaoix/test-self-update
 
 go 1.23
 
-require github.com/bool64/dev v0.2.36
+require github.com/bool64/dev v0.2.39
--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 // Package main is an example CLI app.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/bool64/dev/version"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Printf("%#v\n", version.Module("github.com/xiaoix/test-self-update"))
+
+	_ = http.ListenAndServe("127.0.0.1:8082", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(fmt.Sprintf("%#v\n", version.Module("github.com/xiaoix/test-self-update"))))
+	}))
 }
